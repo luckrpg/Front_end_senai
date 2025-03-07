2ada2706658f24d3c43fe2eeaa067484cbd726f4
luck rpg <ferreiraagerlan@gmail.com>
2025-03-07 12:42:13 -0300

    atividade senai

---
+++ exemplo.js
@@ -0,0 +1,23 @@
+// alert("ola =P")
+//
+// var test = prompt("digita ai")
+//
+// alert(text)
+}function test() {
+    document.getElementById("titulo")
+    titulo.style.color = "red"
+    titulo.style.fontsize = "24px"
+}
+
+function reset() {
+    document.getElementById("titulo").innerHTML = "Himiko toga"
+}
+
+function testinput(){
+    document.getElementById("input").value = "tarde só"
+}
+
+function limpainput(){
+    document.getElementById('input').value = "=P"
+
+}
